587d9e550e8026dc48ea1cdf3dfd939dff8b1ab4
unknown <emirbraz.d2@gmail.com>
2025-10-02 12:55:18 -0300

    handler comments

--- sw/handlers/exception_handler.asm
+++ sw/handlers/exception_handler.asm
@@ -1,40 +1,48 @@
+# Memory map: Compact (MARS), Exception @ 0x00004180
+
+
 # Kernel Data Segment
-.kdata 0x00003000         # base correta no Compact (Text@0)
-flag:   .word 0
+.kdata 0x00005000
+.align 2
+flag:   .word 0       # usado p/ sinalizar IRQ ao user
 
 # Kernel Boot #################################
 .ktext 0x00004000
 __kernel_boot:
+    # Habilita interrupções globais (IE=1) e do timer (IM7=1)
     mfc0   $t0, $12
-    ori    $t0, $t0, 0x8001   # habilita IE e IM7
+    ori    $t0, $t0, 0x8001   # IM7 + IE
+            # 1000 0000 0000 0001 
+    li     $t1, 0xFFFD # 
+            # 1111 1111 1111 1101
+    and    $t0, $t0, $t1      # limpa EXL
     mtc0   $t0, $12
 
-    j main
+    j      main       # pula para user code #########################"main not found error" ###############################
     nop
 
-# Exception Handler ############################
+# Exception Handler############################
 .ktext 0x00004180
 handler:
-    mfc0 $k0, $13
-    srl  $t0, $k0, 2
-    andi $t0, $t0, 0x1F   # ExcCode
+    mfc0 $k0, $13        # Le Cause
+    srl $t0, $k0, 2
+    andi $t0, $t0, 0x1F  # ExcCode (5 bits)
 
-    bne  $t0, $zero, not_interrupt
+    bne    $t0, $zero, not_interrupt
     nop
 
-    # Checa IP7
-    andi $t1, $k0, 0x8000
-    beq  $t1, $zero, not_interrupt
+    # Checa IP7 (bit 15 de Cause)
+    andi   $t1, $k0, 0x8000
+    beq    $t1, $zero, not_interrupt
     nop
 
     # --- Tratamento Timer ---
-    li   $v0, 4
-    la   $a0, msg_irq
-    syscall
+    li     $t2, 4
+    sw     $t2, flag
 
-    mfc0 $t3, $9
-    addiu $t3, $t3, 10000
-    mtc0  $t3, $11        # reprograma Compare
+    mfc0   $t3, $9          # lê Count
+    addiu  $t3, $t3, 10000
+    mtc0   $t3, $11         # escreve Compare (limpa pending)
     eret
 
 not_interrupt:
